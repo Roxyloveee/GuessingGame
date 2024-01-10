ee454b03ab929395b3943afd4dca8bd8dbc9f1e2
username <useremail>
2024-01-09 21:04:56 -0800

    Guessing game code

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,1 +1,32 @@
-
+import random
+def display_title():  
+    print ("Guess the Number!")
+    print()
+def play_game(limit):
+    number=random.randint(1,limit)
+    print(f"I'm thinking of a number between 1 and {limit}\n.")
+    count=1
+    guess=int(input("What is your guess? "))
+    
+    while(guess!=number):
+        if guess>number:
+            print("Too high.")
+            count+=1
+        elif guess<number:
+            print("Too low.")
+            count+=1
+        guess=int(input("Your guess: "))
+            
+    print(f"You guessed it in {count} attempts.\n")
+def main():
+    display_title()
+    again="yes"
+    while again.lower()=="yes":
+        limit=int(input("Enter the limit:"))
+        play_game(limit)
+        again=input("Would you like to play again? Type yes or no: ")
+        print()
+    print("Thanks for playing! See Ya!")
+     
+if __name__=="__main__":
+    main() 
